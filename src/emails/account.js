984803189a6e3e40bcbdf54f3c75342a4e55ee9b
Mihai Ghe <mihaighe@pop-os.localdomain>
2020-04-23 14:59:46 +0300

    Init Commit

---
+++ src/emails/account.js
@@ -0,0 +1,19 @@
+// const sgMail = require('@sendgrid/mail')
+
+// const sendgridAPIkey = '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'
+
+// sgMail.setApiKey(sendgridAPIkey)
+
+// sgMail.send({
+//         to: 'mihai.md16@gmail.com',
+//         from: 'mihai.md16@gmail.com',
+//         subject: 'this is my first mail',
+//         text: 'is it working'
+//     })
+//     .then(() => {}, error => {
+//       console.error(error);
+   
+//       if (error.response) {
+//         console.error(error.response.body)
+//       }
+// });
